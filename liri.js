6809611ac7c57cd266716eea864170e2b59c4598
Bob Brown <robertbernardbrown@gmail.com>
2018-01-28 17:20:50 -0500

    gets data from Spotify

--- liri.js
+++ liri.js
@@ -4,8 +4,8 @@ var keys = require("./keys");
 var Twitter = require('twitter');
 var client = new Twitter(keys.twitter);
 //===========SPOTIFY=============
-// var Spotify = require('spotify');
-// var spotify = new Spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
+var spotifyClient = new Spotify(keys.spotify);
 //===========REQUEST=============
 var requestCall = require('request');
 
@@ -15,24 +15,39 @@ switch (process.argv[2]) {
 
         var params = {
             screen_name: 'Bob_Brown18',
-            count: 10
+            count: 20
         };
-        client.get('statuses/user_timeline', params, function(error, tweets, response) {
-        if (!error) {
-            console.log('Here are your last 10 tweets: ');
-            var counter = 1;
-            for (var i = 0; i < tweets.length; i++) {
-                var element = tweets[i];
-                var twitterText = counter++ +'. ' + element.text
-                console.log(twitterText);
+        client.get('statuses/user_timeline', params, function (error, tweets, response) {
+            if (!error) {
+                console.log('Here are your last 20 tweets: ');
+                var counter = 1;
+                for (var i = 0; i < tweets.length; i++) {
+                    var element = tweets[i];
+                    var twitterText = counter++ + '. ' + element.created_at + ' ' + element.text
+                    console.log(twitterText);
+                }
             }
-        }
         });
         break;
     
     case 'spotify_this_song':
-        console.log('song');
+
+        var params = {
+            type: 'track',
+            query: 'All the Small Things'
+        };
+        spotifyClient.search(params, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            var spotifyArray = data.tracks.items;
+            for (var i = 0; i < spotifyArray.length; i++) {
+                var element = spotifyArray[i];
+                console.log(element);
+            }
+        });
         break;
+
     case 'movie_this':
         console.log('movie');
         break;
