ad2f94813e8192b015e3e174bd6eb916778b7e7e
Bob Brown <robertbernardbrown@gmail.com>
2018-01-28 14:25:22 -0500

    gets data from Twitter

--- liri.js
+++ liri.js
@@ -1,2 +1,22 @@
-require("dotenv").config();
-require("keys.js").config();
+var config = require("dotenv").config();
+var keys = require("./keys");
+//===========TWITTER=============
+var Twitter = require('twitter');
+var client = new Twitter(keys.twitter);
+//===========SPOTIFY=============
+// var Spotify = require('spotify');
+// var spotify = new Spotify(keys.spotify);
+//===========REQUEST=============
+var requestCall = require('request');
+
+
+
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+if (!error) {
+    console.log(tweets);
+}
+});
+
+
+console.log(client)
